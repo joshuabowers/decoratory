ccb2788e8796e7491d9d1f61bdc098749d707cff
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-11-03 16:00:50 -0700

    Testing CRA deployment

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { app as firebase } from './app/firebase';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
